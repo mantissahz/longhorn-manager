93c2def3e903f3ca1385fe7df159a3c05bc05f4f
James Lu <james.lu@suse.com>
2026-04-07 08:54:11 +0800

    feat: enable offline-replica-rebuilding setting as default
    ref: longhorn/longhorn 11204
    
    Signed-off-by: James Lu <james.lu@suse.com>

--- types/setting.go
+++ types/setting.go
@@ -1955,7 +1955,7 @@ var (
 		Required:           true,
 		ReadOnly:           false,
 		DataEngineSpecific: false,
-		Default:            "false",
+		Default:            "true",
 	}
 
 	SettingDefinitionLogPath = SettingDefinition{
